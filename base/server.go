5a69f725ab4590f326cde292fee05851c2d725f1
lvcong <lvcong@ebidsun.com>
2021-11-23 16:54:17 +0800

    init

---
+++ base/server.go
@@ -0,0 +1,12 @@
+/*
+# -*- coding: utf-8 -*-
+# @Author : joker
+# @Time : 2021/11/23 4:51 下午
+# @File : server.go
+# @Description :
+# @Attention :
+*/
+package base
+
+type IServer interface {
+}
---
+++ framework/base/server.go
@@ -0,0 +1,17 @@
+/*
+# -*- coding: utf-8 -*-
+# @Author : joker
+# @Time : 2021/11/23 4:51 下午
+# @File : server.go
+# @Description :
+# @Attention :
+*/
+package base
+
+import (
+	"github.com/itsfunny/go-cell/base"
+)
+
+type IFrameworkServer interface {
+	base.IServer
+}
