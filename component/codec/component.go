f915cda63d25708ee72415ddf7dc175875bcf3a2
itsfunny <18757883747@163.com>
2022-11-08 22:19:24 +0800

    must

--- component/codec/component.go
+++ component/codec/component.go
@@ -41,6 +41,22 @@ func (c *CodecComponent) GetCodec() types.Codec {
 func (c *CodecComponent) Marshal(data interface{}) ([]byte, error) {
 	return c.cdc.Marshal(data)
 }
+
+func (c *CodecComponent) MustMarshal(data interface{}) []byte {
+	ret, err := c.cdc.Marshal(data)
+	if nil != err {
+		panic(err)
+	}
+	return ret
+}
+
+func (c *CodecComponent) MustUnMarshal(data []byte, ret interface{}) {
+	err := c.cdc.Unmarshal(data, ret)
+	if nil != err {
+		panic(err)
+	}
+}
+
 func (c *CodecComponent) UnMarshal(data []byte, ret interface{}) error {
 	return c.cdc.Unmarshal(data, ret)
 }
