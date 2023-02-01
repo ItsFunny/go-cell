acc50d37afadf401ba0192a2322ce129e1fe59de
itsfunny <18757883747@163.com>
2023-02-01 17:50:01 +0800

    zk

--- demo/zk/main.go
+++ demo/zk/main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"fmt"
 	gocircom "github.com/iden3/go-circom-witnesscalc"
+	"github.com/iden3/go-wasm3"
 	"io/ioutil"
 )
 
 func main() {
+	runtime := wasm3.NewRuntime(&wasm3.Config{
+		Environment: wasm3.NewEnvironment(),
+		StackSize:   64 * 1024,
+	})
+	defer runtime.Destroy()
+
 	wasmBytes, err := ioutil.ReadFile("./circuit.wasm")
+	noErr(err)
+
+	module, err := runtime.ParseModule(wasmBytes)
+	noErr(err)
+	module, err = runtime.LoadModule(module)
 	noErr(err)
 
 	inputBytes, err := ioutil.ReadFile("./input.json")
 	noErr(err)
 
-	calc, err := gocircom.NewCircom2WitnessCalculator(wasmBytes, true)
+	calc, err := gocircom.NewWitnessCalculator(runtime, module)
 	noErr(err)
 
 	inputs, err := gocircom.ParseInputs(inputBytes)
